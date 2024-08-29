7276303638ba2235c4a55e24c149037ba57fee5c
farhannovaldi <farhannovaldi002@gmail.com>
2024-08-30 01:16:00 +0700

    upgrade

--- index.php
+++ index.php
@@ -10,27 +10,28 @@
 
 <body>
     <header>
-        <h1>Farhan Novaldi</h1>
-        <p>Portfolio - Apple Developer Academy 2025</p>
+        <div class="container">
+            <h1>Farhan Novaldi</h1>
+            <p>Portfolio - Apple Developer Academy 2025</p>
+        </div>
     </header>
 
     <section id="introduction">
-        <h2>Introduction</h2>
-        <p>I am eager to join the Apple Developer Academy because I see it as an opportunity to deepen my knowledge in application development and digital technology. I have a strong desire to keep learning, especially in a field that is constantly evolving.
-            The Academy provides an environment that supports intensive and practical learning, which I believe is crucial for honing my skills.
-            Additionally, I am excited about the prospect of expanding my professional network by connecting and collaborating with talented individuals from diverse backgrounds.
-            I believe that interacting with people who share the same passion and drive will open up new opportunities and enrich my learning experience. By joining the Academy, I hope to grow professionally, expand my connections, and contribute meaningfully to the tech industry.</p>
-        <section id="projects">
+        <div class="container">
+            <h2>Introduction</h2>
+            <p>I am eager to join the Apple Developer Academy because I see it as an opportunity to deepen my knowledge in application development and digital technology. I have a strong desire to keep learning, especially in a field that is constantly evolving. The Academy provides an environment that supports intensive and practical learning, which I believe is crucial for honing my skills. Additionally, I am excited about the prospect of expanding my professional network by connecting and collaborating with talented individuals from diverse backgrounds. I believe that interacting with people who share the same passion and drive will open up new opportunities and enrich my learning experience. By joining the Academy, I hope to grow professionally, expand my connections, and contribute meaningfully to the tech industry.</p>
+        </div>
+    </section>
+
+    <section id="projects">
+        <div class="container">
             <h2>Projects</h2>
 
-            <!-- Example of a project item -->
             <div class="project-item">
                 <h3>Jobs Finder</h3>
-                <p><strong>Summary:</strong> This project is a job seeker application developed using Flutter, designed to help users create an account, upload their CV, and manage job applications
-                    . Users can save job postings of interest, apply for jobs either directly or from their saved list, and track the status of their applications while waiting for responses from employers.</p>
+                <p><strong>Summary:</strong> This project is a job seeker application developed using Flutter, designed to help users create an account, upload their CV, and manage job applications. Users can save job postings of interest, apply for jobs either directly or from their saved list, and track the status of their applications while waiting for responses from employers.</p>
                 <p><strong>Type:</strong> Class Assignment (Final Semester Exam)</p>
-                <p><strong>Learning:</strong> Working on this project enhanced my skills in Flutter development and integrating Firebase services, including authentication, real-time database, Firestore database, and storage.
-                    It also provided practical experience in designing user interfaces and managing application states.</p>
+                <p><strong>Learning:</strong> Working on this project enhanced my skills in Flutter development and integrating Firebase services, including authentication, real-time database, Firestore database, and storage. It also provided practical experience in designing user interfaces and managing application states.</p>
                 <p><strong>Technologies Used:</strong> Flutter, Firebase Authentication, Firebase Realtime Database, Firestore Database, Firebase Storage.</p>
                 <div class="project-images">
                     <img src="assets/job1.png" alt="Project Image">
@@ -39,8 +40,9 @@
                     <img src="assets/job4.png" alt="Project Image">
                     <img src="assets/job5.png" alt="Project Image">
                 </div>
-                <a href="https://github.com/farhannovaldi/jobsfinder" target="_blank">View Project</a>
+                <a href="https://github.com/farhannovaldi/jobsfinder" target="_blank" class="view-project-btn">View Project</a>
             </div>
+
             <div class="project-item">
                 <h3>Rasa Peduli</h3>
                 <p><strong>Summary:</strong> This project is an Android application developed using Android Studio. The app helps users find recommended food items for donation, locate donation centers, view lists of donors, access documentation, and register to make donations. It streamlines the donation process by providing users with a comprehensive platform for managing and participating in food donations.</p>
@@ -55,25 +57,30 @@
                     <img src="assets/rasa5.png" alt="Project Image">
                     <img src="assets/rasa6.png" alt="Project Image">
                 </div>
-                <a href="https://github.com/farhannovaldi/AplikasiDonasiMakanan" target="_blank">View Project</a>
+                <a href="https://github.com/farhannovaldi/AplikasiDonasiMakanan" target="_blank" class="view-project-btn">View Project</a>
             </div>
+
             <div class="project-item">
                 <h3>Country Data Application</h3>
                 <p><strong>Summary:</strong> The Country Data Application is a web app that allows users to search and view detailed information about various countries. Users can select a country from a dropdown menu and view information such as the country's name, capital, population, area, and flag.</p>
-                <p><strong>Type:</strong> Class Assignment </p>
+                <p><strong>Type:</strong> Class Assignment</p>
                 <p><strong>Learning:</strong> Developing this application enhanced my skills in using Express.js to build server-side web applications, Handlebars (hbs) for rendering dynamic pages, and MongoDB for managing user data and sessions. I also learned how to implement session-based authentication and manage user state in a web application.</p>
                 <p><strong>Technologies Used:</strong> Node.js, Express.js, Handlebars (hbs), MongoDB, Session Management.</p>
                 <div class="project-images">
                     <img src="assets/data1.png" alt="Project Image">
                     <img src="assets/data2.png" alt="Project Image">
                 </div>
-                <a href="https://github.com/farhannovaldi/Aplikasi-Data-Negara" target="_blank">View Project</a>
+                <a href="https://github.com/farhannovaldi/Aplikasi-Data-Negara" target="_blank" class="view-project-btn">View Project</a>
             </div>
-        </section>
-        <footer>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
             <p>Contact me at: <a href="mailto:farhannovaldi002@gmail.com">farhannovaldi002@gmail.com</a></p>
             <p>Find me on <a href="https://www.linkedin.com/in/farhan-novaldi-08b091326/" target="_blank">LinkedIn</a> | <a href="https://github.com/farhannovaldi" target="_blank">GitHub</a></p>
-        </footer>
+        </div>
+    </footer>
 </body>
 
 </html>
